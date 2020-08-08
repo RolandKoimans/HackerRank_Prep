653b411b789ee275bdf2d2bffb951b010f688de4
Roland Koimans <roland.koimans@gmail.com>
2020-08-08 20:39:07 +0200

    Solution for Minimum Swaps 2

---
+++ Interview Preperation Kit/Arrays/Minimum Swaps 2/Minimum Swaps 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr)
+    {
+        int count = 0;
+        for(int i = 0; i<arr.Length; i++)
+        {
+            //While the current place doesn't have the correct number, swap the number in this place to the place where it belongs.
+            //Note that this only works because all the numbers are consecutive starting at 1.
+            while(arr[i] != i + 1)
+            {
+                int a = arr[i];
+                int b = arr[a - 1];
+
+                arr[i] = b;
+                arr[a - 1] = a;
+
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int res = minimumSwaps(arr);
+
+        textWriter.WriteLine(res);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
