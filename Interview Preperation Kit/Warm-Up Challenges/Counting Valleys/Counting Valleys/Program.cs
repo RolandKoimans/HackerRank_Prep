ecd36d2f41e9f43d100e598933e0893624f83b20
Roland Koimans <roland.koimans@gmail.com>
2020-08-09 17:17:34 +0200

    Solution to Counting Valleys

---
+++ Interview Preperation Kit/Warm-Up Challenges/Counting Valleys/Counting Valleys/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, string s)
+    {
+        int valleys = 0;
+
+        char[] steps = s.ToCharArray();
+
+        int height = 0;
+        for(int i = 0; i < steps.Length; i++)
+        {
+            if (height == 0 && steps[i] == 'D')
+                valleys++;
+
+            if (steps[i] == 'D')
+                height--;
+            if (steps[i] == 'U')
+                height++;
+        }
+
+
+        return valleys;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string s = Console.ReadLine();
+
+        int result = countingValleys(n, s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
