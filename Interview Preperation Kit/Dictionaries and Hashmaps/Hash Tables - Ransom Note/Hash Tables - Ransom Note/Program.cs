2a4763f7d3b20bd8cbe5088edf1879559c805f03
Roland Koimans <roland.koimans@gmail.com>
2020-08-10 23:46:05 +0200

    Solution for Ransom Note

---
+++ Interview Preperation Kit/Dictionaries and Hashmaps/Hash Tables - Ransom Note/Hash Tables - Ransom Note/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(string[] magazine, string[] note)
+    {
+
+        Hashtable magazineTable = new Hashtable();
+
+        foreach(string str in magazine)
+        {
+            if (!magazineTable.ContainsKey(str))
+            {
+                magazineTable.Add(str, 1);
+            }
+            else
+            {
+                magazineTable[str] = (int)magazineTable[str] + 1;
+            }
+        }
+
+
+        foreach(string str in note)
+        {
+            if (!magazineTable.ContainsKey(str))
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            else if((int)magazineTable[str] == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            else
+            {
+                magazineTable[str] = (int)magazineTable[str] - 1;
+            }
+        }
+
+        Console.WriteLine("Yes");
+
+    }
+
+    static void Main(string[] args)
+    {
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        string[] magazine = Console.ReadLine().Split(' ');
+
+        string[] note = Console.ReadLine().Split(' ');
+
+        checkMagazine(magazine, note);
+    }
+}
