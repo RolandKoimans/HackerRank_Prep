40df22aa009967d60b17a52f9cbd864ac5e2b6bb
Roland Koimans <roland.koimans@gmail.com>
2020-08-10 13:14:15 +0200

    Solution for Jumping on the Clouds

---
+++ Interview Preperation Kit/Warm-Up Challenges/Jumping on the Clouds/Jumping on the Clouds/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c)
+    {
+
+        int jumps = 0;
+
+        int pos = 0;
+        while (pos < c.Length-2)
+        {
+            if (c[pos + 2] == 0)
+            {
+                pos += 2;
+                jumps++;
+            }
+            else
+            {
+                pos++;
+                jumps++;
+            }
+        }
+
+        if(pos == c.Length - 2)
+        {
+            jumps++;
+        }
+
+        return jumps;
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+        ;
+        int result = jumpingOnClouds(c);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
