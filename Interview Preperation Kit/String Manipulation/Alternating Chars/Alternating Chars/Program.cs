59a4894328da89115e31caf2be061fbc905c9ef6
Roland Koimans <roland.koimans@gmail.com>
2020-08-21 10:22:52 +0200

    Solution for Alternating Characters

---
+++ Interview Preperation Kit/String Manipulation/Alternating Chars/Alternating Chars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alternating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
